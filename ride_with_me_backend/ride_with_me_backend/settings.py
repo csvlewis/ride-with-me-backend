22c01a5e7c511c66bf7bb609c248c19a3fcb5548
Teresa Knowles <teresa07moreno@gmail.com>
2019-05-20 22:32:05 -0600

    Add settings for Graphene

--- ride_with_me_backend/ride_with_me_backend/settings.py
+++ ride_with_me_backend/ride_with_me_backend/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'phonenumber_field'
+    'phonenumber_field',
+    'graphene_django',
+
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GRAPHENE = {
+    'SCHEMA': 'ride_with_me_backend.schema.schema',
+}
