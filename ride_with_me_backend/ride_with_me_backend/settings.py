e3089da9669de3861436d3cc75da9231a0cd960e
Teresa Knowles <teresa07moreno@gmail.com>
2019-05-20 18:35:11 -0600

    Add CSV file

--- ride_with_me_backend/ride_with_me_backend/settings.py
+++ ride_with_me_backend/ride_with_me_backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'csvimport.app.CSVImportConf',
     'rides.apps.RidesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
