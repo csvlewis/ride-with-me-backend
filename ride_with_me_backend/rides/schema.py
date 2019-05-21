20797c656c4dc29accace52990297378870347a3
Teresa Knowles <teresa07moreno@gmail.com>
2019-05-20 22:33:10 -0600

    Add all_cities query

--- ride_with_me_backend/rides/schema.py
+++ ride_with_me_backend/rides/schema.py
@@ -1,16 +1,25 @@
 import graphene
 from graphene_django import DjangoObjectType
+from django.db.models.functions import Concat
+from django.db.models import Value
 
 from .models import City
 
 
 class CityType(DjangoObjectType):
+    name = graphene.String()
+
     class Meta:
         model = City
 
+    def resolve_name(self, info):
+        if self is not None:
+            return '%s, %s' % (self.city, self.state)
+        return ''
+
 
 class Query(graphene.ObjectType):
-    cities = graphene.List(CityType)
+    all_cities = graphene.List(CityType)
 
-    def resolve_cities(self, info, **kwargs):
+    def resolve_all_cities(self, info, **kwargs):
         return City.objects.all()
