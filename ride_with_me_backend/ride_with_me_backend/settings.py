fd8ae61356a4b59eb247d9a040a4ed9fb307d61d
Chris Lewis <13chlewis@gmail.com>
2019-05-21 13:59:52 -0600

    Remove unnecessary app from settings.py

--- ride_with_me_backend/ride_with_me_backend/settings.py
+++ ride_with_me_backend/ride_with_me_backend/settings.py
@@ -39,9 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'phonenumber_field',
     'graphene_django',
-
 ]
 
 MIDDLEWARE = [
