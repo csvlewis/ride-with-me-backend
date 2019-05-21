072695d3370f8fc81ce0d90b9bc3d2d4e6a21404
Teresa Knowles <teresa07moreno@gmail.com>
2019-05-20 20:56:54 -0600

    Delete name from city

--- ride_with_me_backend/rides/models.py
+++ ride_with_me_backend/rides/models.py
@@ -13,7 +13,6 @@ class City(models.Model):
     def __str__(self):
         return '%s, %s' % (self.city, self.state)
 
-
 class User(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
