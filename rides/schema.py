37a5f3b40b8045ecf24a09eb7138f798cc7c9f47
Teresa Knowles <teresa07moreno@gmail.com>
2019-05-29 10:22:37 -0600

    Add fetch call to Distance Matrix api

--- rides/schema.py
+++ rides/schema.py
@@ -83,10 +83,27 @@ class CreateRide(graphene.Mutation):
         total_seats = graphene.Int()
         departure_time = graphene.types.datetime.Date()
 
-    def mutate(self, info, driver_uuid, start_city_id, end_city_id, description, mileage, price, total_seats, departure_time):
+    def mutate(self, info, driver_uuid, start_city_id, end_city_id, description, price, total_seats, departure_time):
+        start_city = City.objects.get(pk=start_city_id)
+        end_city = City.objects.get(pk=end_city_id)
+        request = requests.get('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=%s+%s&destinations=%s+%s&key=%s' % (str(start_city.city), str(start_city.state), str(end_city.city), str(end_city.state), GOOGLE_MAPS_API_KEY))
+        response = request.json()
+        mileage = int(round(response["rows"][0]["elements"][0]["distance"]["value"] * 0.00062137))
+        travel_time = response["rows"][0]["elements"][0]["duration"]["text"]
+
         driver = User.objects.filter(uuid = driver_uuid)
         if driver[0].uuid == driver_uuid:
-            ride = Ride(driver_id=driver[0].id, start_city_id=start_city_id, end_city_id=end_city_id, description=description, mileage=mileage, price=price, total_seats=total_seats, departure_time=departure_time, status='available')
+            ride = Ride(
+                        driver_id=driver[0].id,
+                        start_city_id=start_city_id,
+                        end_city_id=end_city_id,
+                        description=description,
+                        mileage=mileage,
+                        travel_time=travel_time,
+                        price=price,
+                        total_seats=total_seats,
+                        departure_time=departure_time,
+                        status='available')
             ride.save()
 
         return CreateRide(ride=ride)
